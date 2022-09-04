5a1b7e5f7c1dde61a7fd901b41993eed805b8f49
mansour90 <mansour_alaa82@yahoo.ro>
2022-09-04 19:49:31 +0300

    update script.js

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// let firstCard = 10;
+// let secondCard = 7;
+
+// let sum = firstCard + secondCard + 6;
+// console.log(sum)  
+
+// if (sum < 21) {
+//   console.log("Do you want to draw a new card? 🙂")
+// }else if(sum === 21) {
+//   console.log("Wohoo! You've got BlackJack 🥳")
+// }else{
+//   console.log("You're out of the game! 😭")
+// }
+
+// =============================================
+
+// let age = 22;
+
+// if(age <= 20) {
+  //   console.log("You can not enter the club!")
+  // }else {
+    //   console.log("Welcome")
+    // }
+
+// =============================================
+
+let age = 100;
+
+if(age < 100) {
+  console.log("Not elegible")
+}else if (age === 100) {
+  console.log("Here is your birthday card from the King!")
+}else {
+  console.log("Not elegible, you have already gotten one")
+}
